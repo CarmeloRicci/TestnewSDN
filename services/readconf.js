74d53abccf1df5e546bb8701bf872b900930ccde
Carmelo Ricci <carmelo-93@hotmail.it>
2020-07-09 22:00:53 +0200

    copnf

--- services/readconf.js
+++ services/readconf.js
@@ -7,20 +7,44 @@ var confnode = [];
 console.log("Start readconf.js\n")
 
 
-const readInterface = readline.createInterface({
-    input: fs.createReadStream('/home/pi/conf.txt'),
-    output: process.stdout,
-    console: false
-});
 
-readInterface.on('line', function(line) {
-    //console.log(line);
+LineReaderSync = require("line-reader-sync")
+lrs = new LineReaderSync("/home/pi/conf.txt")
+while(true){
+  var line = lrs.readline()
+  if(line === null){
+    console.log("EOF");
+  }else{
+    //console.log("line without \n",line)
     var w = line.split("\t");
     confnode.push({
             properties : w[0],
             values: w[1]
         });
-});
+  }
+  
+}
+
+
+
+
+
+
+
+// const readInterface =  readline.createInterface({
+//     input: fs.createReadStream('/home/pi/conf.txt'),
+//     output: process.stdout,
+//     console: false
+// });
+
+//  readInterface.on('line', function(line) {
+//     //console.log(line);
+//     var w = line.split("\t");
+//     confnode.push({
+//             properties : w[0],
+//             values: w[1]
+//         });
+// });
 
 //     lineReader.eachLine('/home/pi/conf.txt', function(line) {
 //     //console.log(line);
