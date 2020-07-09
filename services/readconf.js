e0c109e36f3629010fc629961ca697af28f0c43d
Carmelo Ricci <carmelo-93@hotmail.it>
2020-07-09 21:21:19 +0200

    conf

--- services/readconf.js
+++ services/readconf.js
@@ -1,12 +1,14 @@
 const lineReader = require('line-reader');
 var r = [];
 
+console.log("Start readconf.js\n")
+
 lineReader.eachLine('/home/pi/conf.txt', function(line) {
     //console.log(line);
         var w = line.split("\t");
         r.push({
-            'properties' : w[0],
-            'values': w[1]
+            properties : w[0],
+            values: w[1]
         });
 });
 console.log(r)
