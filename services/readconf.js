655d21cc0d1e01aefe93417eade71c6032e64039
Carmelo Ricci <carmelo-93@hotmail.it>
2020-07-09 22:48:59 +0200

    conf

--- services/readconf.js
+++ services/readconf.js
@@ -2,7 +2,8 @@ const lineReader = require('line-reader');
 const fs = require('fs');
 const readline = require('readline');
 
-var confnode = [];
+var confnode = Object.create( {} );
+//var confnode = [];
 
 console.log("Start readconf.js\n")
 
@@ -13,12 +14,29 @@ try {
 
     lines.forEach((line) => {
         var w = line.split("\t");
-        var tmp = JSON.parse('{"'+ w[0] + '" : ' + w[1] + ' }')
-        confnode.push(tmp);
+        var tmp = { [ w[0] ]: w[1] }
+        confnode.push(tmp)
     });
 } catch (err) {
     console.error(err);
 }
 
 exports.confnode = confnode;
+
+
+// try {
+//     const data = fs.readFileSync('/home/pi/conf.txt', 'UTF-8');
+
+//     const lines = data.split('\n');
+
+//     lines.forEach((line) => {
+//         var w = line.split("\t");
+//         var tmp = JSON.parse('{"'+ w[0] + '" : ' + w[1] + ' }')
+//         confnode.push(tmp);
+//     });
+// } catch (err) {
+//     console.error(err);
+// }
+
+// exports.confnode = confnode;
 
