f2624cff33435c6fead3eded9e53954f7ad39b45
Carmelo Ricci <carmelo-93@hotmail.it>
2020-07-09 21:01:47 +0200

    conf

--- main.js
+++ main.js
@@ -14,17 +14,18 @@ fs.readFile('/home/pi/conf.txt', 'utf8', function (err,data) {
     return console.log(err);
   }
   console.log(data);
-  text=data
 });
+
 console.log(text)
-var v = text.split("\n");
-for (var i = 0; i < v.length; i++) {
-    var w = v[i].split("\t");
-    var tmp = w[0]
-    r.push({
-        tmp: w[1]
-    });
-  }
+
+// var v = text.split("\n");
+// for (var i = 0; i < v.length; i++) {
+//     var w = v[i].split("\t");
+//     var tmp = w[0]
+//     r.push({
+//         tmp: w[1]
+//     });
+//   }
 
 
 
