5b60de068d1a2f24e3d32c6f783e2505b5b076cc
Carmelo Ricci <carmelo-93@hotmail.it>
2020-07-09 22:12:57 +0200

    conf

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ const ModuleReadConf = require('./services/readconf');
 
 
 //console.log(ModuleGetIp.ArrayIpOut)
-console.log(ModuleReadConf.confnode)
+var NodeConf = ModuleReadConf.confnode;
+console.log('\n\n\t\t\t Sono il nodo ' + NodeConf.properties['MyAddress']+' \n\n')
 
 //ModuleListenerC.StartListener("Client Mode",ModuleGetIp.IpClient,2222);
 //ModuleListenerS.StartListener("Station Mode",ModuleGetIp.IpServer,50007);
--- services/readconf.js
+++ services/readconf.js
@@ -7,13 +7,10 @@ var confnode = [];
 console.log("Start readconf.js\n")
 
 try {
-    // read contents of the file
     const data = fs.readFileSync('/home/pi/conf.txt', 'UTF-8');
 
-    // split the contents by new line
     const lines = data.split('\n');
 
-    // print all lines
     lines.forEach((line) => {
         var w = line.split("\t");
         confnode.push({
@@ -24,32 +21,6 @@ try {
 } catch (err) {
     console.error(err);
 }
-
-
-
-// const readInterface =  readline.createInterface({
-//     input: fs.createReadStream('/home/pi/conf.txt'),
-//     output: process.stdout,
-//     console: false
-// });
-
-//  readInterface.on('line', function(line) {
-//     //console.log(line);
-//     var w = line.split("\t");
-//     confnode.push({
-//             properties : w[0],
-//             values: w[1]
-//         });
-// });
-
-//     lineReader.eachLine('/home/pi/conf.txt', function(line) {
-//     //console.log(line);
-//         var w = line.split("\t");
-//         r.push({
-//             properties : w[0],
-//             values: w[1]
-//         });
-// });
 
 exports.confnode = confnode;
 
