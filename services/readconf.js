cf8dd6c3af29934bfab9625822811b49c867ab78
Carmelo Ricci <carmelo-93@hotmail.it>
2020-07-09 21:51:21 +0200

    conf

--- services/readconf.js
+++ services/readconf.js
@@ -1,16 +1,37 @@
 const lineReader = require('line-reader');
+const fs = require('fs');
+const readline = require('readline');
+
 var r = [];
 
 console.log("Start readconf.js\n")
 
-lineReader.eachLine('/home/pi/conf.txt', function(line) {
+
+const readInterface = readline.createInterface({
+    input: fs.createReadStream('/home/pi/conf.txt'),
+    output: process.stdout,
+    console: false
+});
+
+readInterface.on('line', function(line) {
     //console.log(line);
-        var w = line.split("\t");
+    var w = line.split("\t");
         r.push({
             properties : w[0],
             values: w[1]
         });
 });
+
+//     lineReader.eachLine('/home/pi/conf.txt', function(line) {
+//     //console.log(line);
+//         var w = line.split("\t");
+//         r.push({
+//             properties : w[0],
+//             values: w[1]
+//         });
+// });
+
+
 exports.lineReader = lineReader;
 exports.confnode = r;
 
