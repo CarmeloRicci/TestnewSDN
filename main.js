e5a510d728f54ea786b59641b47c5f570a492b09
Carmelo Ricci <carmelo-93@hotmail.it>
2020-07-09 20:46:26 +0200

    ok

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 var ModuleListenerC = require('./services/listener');
 var ModuleListenerS = require('./services/listenerS');
 var ModuleGetIp = require('./services/GetIp');
+var ModuleReadConf = require('./services/readconf');
 
 console.log(ModuleGetIp.ArrayIpOut)
+ModuleReadConf.readfile
 
 ModuleListenerC.StartListener("Client Mode",ModuleGetIp.IpClient,2222);
 ModuleListenerS.StartListener("Station Mode",ModuleGetIp.IpServer,50007);
