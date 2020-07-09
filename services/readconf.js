3c14920e33e739de8c31ea475fadbd91175580af
Carmelo Ricci <carmelo-93@hotmail.it>
2020-07-09 20:57:34 +0200

    conf

--- services/readconf.js
+++ services/readconf.js
@@ -1,10 +1,24 @@
 fs = require('fs')
 
+var text;
+var r = [];
+
 fs.readFile('/home/pi/conf.txt', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
   console.log(data);
+  text=data
 });
 
+var v = t.split("\n");
+for (var i = 0; i < v.length; i++) {
+    var w = v[i].split("\t");
+    var tmp = w[0]
+    r.push({
+        tmp: w[1]
+    });
+  }
+
 exports.readfile = fs.readFile;
+exports.v = v;
