85752bbad98c77d4d5d4a5c0645a1c8b055878d9
Carmelo Ricci <carmelo-93@hotmail.it>
2020-07-10 12:20:21 +0200

    ok

--- main.js
+++ main.js
@@ -5,19 +5,19 @@ const ModuleReadConf = require('./services/readconf');
 
 async function start() {
 
-var NodeConf = await ModuleReadConf.confnode; // map con tutti i parametri letti dal file
+    var NodeConf = await ModuleReadConf.confnode; // map con tutti i parametri letti dal file
 
-console.log('\n\n\t\tI am node ' + NodeConf.get('MyAddress') +'\n\t\tMy Client Address is: '+NodeConf.get('ClientIp')+'\n\t\tMy Server Address is: '+NodeConf.get('ServerIp')+ '\n')
+    console.log('\n\n\t\tI am node ' + NodeConf.get('MyAddress') + '\n\t\tMy Client Address is: ' + NodeConf.get('ClientIp') + '\n\t\tMy Server Address is: ' + NodeConf.get('ServerIp') + '\n')
 
+    console.log(NodeConf)
 
-
-if ( NodeConf.get('SINK') == 'YES') {
-    console.log('\n\t\t I am the Sink')
-    await ModuleListenerC.StartListener("Client Mode",NodeConf);
-} else{
-    console.log('\n\t\t I am a Node')
-    await ModuleListenerS.StartListener("Station Mode",NodeConf);
-}
+    if (NodeConf.get('SINK') == 'YES') {
+        console.log('\n\t\t I am the Sink')
+        await ModuleListenerC.StartListener("Client Mode", NodeConf);
+    } else {
+        console.log('\n\t\t I am a Node')
+        await ModuleListenerS.StartListener("Station Mode", NodeConf);
+    }
 
 
 
