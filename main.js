cc61f6505f1f49337618aad421fdf738f10804fc
Carmelo Ricci <carmelo-93@hotmail.it>
2020-07-10 08:55:29 +0200

    ok

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 const ModuleListenerC = require('./services/listener');
-const ModuleListenerS = require('./services/listenerS');
+const ModuleListenerS = require('./services/listener');
 const ModuleGetIp = require('./services/GetIp');
-const ModuleReadConf = require('./services/readconf');
+const ModuleReadConf = require('./services/Readconf');
 
 async function start() {
 
 var NodeConf = await ModuleReadConf.confnode; // map con tutti i parametri letti dal file
 
-console.log('\n\n\t\tI am node ' + NodeConf.get('MyAddress').bold() +'\n\t\tMy Client Address is: '+NodeConf.get('ClientIp')+'\n\t\tMy Server Address is: '+NodeConf.get('ServerIp')+ '\n')
+console.log('\n\n\t\tI am node ' + NodeConf.get('MyAddress') +'\n\t\tMy Client Address is: '+NodeConf.get('ClientIp')+'\n\t\tMy Server Address is: '+NodeConf.get('ServerIp')+ '\n')
 
-await ModuleListenerC.StartListener("Client Mode",ModuleGetIp.IpClient,2222);
-await ModuleListenerS.StartListener("Station Mode",ModuleGetIp.IpServer,50007);
+await ModuleListenerC.StartListener("Client Mode",NodeConf.get('ClientIp'),2222);
+await ModuleListenerS.StartListener("Station Mode",NodeConf.get('ServerIp'),50007);
+
+
 
 }
-
-
-
 
 start().then
