14977751b02cb1c57e31d6e813cfc042270b8a7b
Carmelo Ricci <carmelo-93@hotmail.it>
2020-07-10 17:14:32 +0200

    ok

--- interfaces/config.js
+++ interfaces/config.js
@@ -10,15 +10,15 @@ exports.LenNextHop = 16;
 exports.LenPayload = 1000;
 
 var TypeBeacon = Buffer.alloc(LenType);
-exports.TypeBeacon = TypeBeacon.write(0);
+exports.TypeBeacon = TypeBeacon.write(''+0);
 var TypeReport = Buffer.alloc(LenType);
-exports.TypeReport = TypeReport.write(1);
+exports.TypeReport = TypeReport.write(''+1);
 var TypeOpenPath = Buffer.alloc(LenType);
-exports.TypeOpenPath= TypeOpenPath.write(2);
+exports.TypeOpenPath= TypeOpenPath.write(''+2);
 var TypeStatus = Buffer.alloc(LenType);
-exports.TypeStatus = TypeStatus.write(3);
+exports.TypeStatus = TypeStatus.write(''+3);
 var TypeData = Buffer.alloc(LenType);
-exports.TypeData = TypeData.write(4);
+exports.TypeData = TypeData.write(''+4);
 
 
 // SdnWise timer (second)
