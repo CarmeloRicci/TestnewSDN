acb4edb9f4307e475f759ade857838ca3d25189a
Carmelo Ricci <carmelo-93@hotmail.it>
2020-07-10 08:43:48 +0200

    ok

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ const ModuleListenerS = require('./services/listenerS');
 const ModuleGetIp = require('./services/GetIp');
 const ModuleReadConf = require('./services/readconf');
 
+async function start() {
 
-var NodeConf = ModuleReadConf.confnode; // map con tutti i parametri letti dal file
-
+var NodeConf = await ModuleReadConf.confnode; // map con tutti i parametri letti dal file
 
 console.log('\n\n\t\t\t Sono il nodo ' + NodeConf.get('MyAddress') +' \n\n')
 
-//ModuleListenerC.StartListener("Client Mode",ModuleGetIp.IpClient,2222);
-//ModuleListenerS.StartListener("Station Mode",ModuleGetIp.IpServer,50007);
+ModuleListenerC.StartListener("Client Mode",ModuleGetIp.IpClient,2222);
+ModuleListenerS.StartListener("Station Mode",ModuleGetIp.IpServer,50007);
 
+}
--- services/readconf.js
+++ services/readconf.js
@@ -2,7 +2,6 @@ const lineReader = require('line-reader');
 const fs = require('fs');
 const readline = require('readline');
 
-//var confnode = [];
 var confnode = new Map();
 
 console.log("Start readconf.js\n")
@@ -20,21 +19,3 @@ try {
 }
 
 exports.confnode = confnode;
-
-
-// try {
-//     const data = fs.readFileSync('/home/pi/conf.txt', 'UTF-8');
-
-//     const lines = data.split('\n');
-
-//     lines.forEach((line) => {
-//         var w = line.split("\t");
-//         var tmp = JSON.parse('{"'+ w[0] + '" : ' + w[1] + ' }')
-//         confnode.push(tmp);
-//     });
-// } catch (err) {
-//     console.error(err);
-// }
-
-// exports.confnode = confnode;
-
