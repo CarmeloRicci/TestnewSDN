5d06163b392a3416c7d3acd14d6f49f32daf758d
Carmelo Ricci <carmelo-93@hotmail.it>
2020-07-10 14:43:07 +0200

    ok

--- interfaces/message.js
+++ interfaces/message.js
@@ -12,13 +12,13 @@ class Message {
 
     static get_message_for_paket (P){
         var BNetId =  new Buffer.from(ModuleConf.LenNetId,''+[P.NetId],'utf8')
-        var BLength =  new Buffer.from(ModuleConf.LenLength,''+[P.Length])
-        var BDestination =  new Buffer.from(ModuleConf.LenDestination,''+[P.Destination])
-        var BSource =  new Buffer.from(ModuleConf.LenSource,''+[P.Source])
-        var BType =  new Buffer.from(ModuleConf.LenType,''+[P.Type])
-        var BTTL =  new Buffer.from(ModuleConf.LenTTL,''+[P.TTL])
-        var BNextHop =  new Buffer.from(ModuleConf.LenNextHop,''+[P.NextHop])
-        var BPayload =  new Buffer.from(ModuleConf.LenPayload,''+[P.Payload])
+        var BLength =  new Buffer.from(ModuleConf.LenLength,''+[P.Length],'utf8')
+        var BDestination =  new Buffer.from(ModuleConf.LenDestination,''+[P.Destination],'utf8')
+        var BSource =  new Buffer.from(ModuleConf.LenSource,''+[P.Source],'utf8')
+        var BType =  new Buffer.from(ModuleConf.LenType,''+[P.Type],'utf8')
+        var BTTL =  new Buffer.from(ModuleConf.LenTTL,''+[P.TTL],'utf8')
+        var BNextHop =  new Buffer.from(ModuleConf.LenNextHop,''+[P.NextHop],'utf8')
+        var BPayload =  new Buffer.from(ModuleConf.LenPayload,''+[P.Payload],'utf8')
         //console.log(BNetId.toString(), BLength.toString(), BDestination.toString(), BSource.toString(), BType.toString(), BTTL.toString(), BNextHop.toString(), BPayload.toString())
         //console.log(BNetId.length, BLength.length, BDestination.length , BSource.length , BType.length , BTTL.length ,BNextHop.length , BPayload.length)
 
