dc279783efd4757889847071ccc115e35a05f9ab
Carmelo Ricci <carmelo-93@hotmail.it>
2020-07-10 10:21:42 +0200

    ok

--- services/listener.js
+++ services/listener.js
@@ -30,11 +30,11 @@ var StartListener = function (TypeListener, NodeConf) {
         var buf = Buffer.from(message);
     });
 
-    
-    const p1 = new Packet.Packets(1,100,1,1,0,99,1,"Ciao");
-    console.log(TypeListener + ':' + p1)
-    Packet.Packets.print_packets(p1);
     server.bind(Port, Ip);
+    const p1 = new Packet.Packets(1,100,1,1,0,99,1,"Ciao");
+    console.log(TypeListener + ':' + p1.Destination)
+    Packet.Packets.print_packets(p1);
+
 }
 
 exports.StartListener = StartListener;
