535245f432078a17684fdc9d14b1a3d9c73abafb
Carmelo Ricci <carmelo-93@hotmail.it>
2020-07-10 08:34:23 +0200

    conf

--- main.js
+++ main.js
@@ -7,64 +7,9 @@
 
 //console.log(ModuleGetIp.ArrayIpOut)
 //var NodeConf = ModuleReadConf.confnode;
-//console.log(ModuleReadConf.confnode)
+console.log(ModuleReadConf.confnode)
 //console.log('\n\n\t\t\t Sono il nodo ' + NodeConf.properties +' \n\n')
 
 //ModuleListenerC.StartListener("Client Mode",ModuleGetIp.IpClient,2222);
 //ModuleListenerS.StartListener("Station Mode",ModuleGetIp.IpServer,50007);
 
-
-  var addToObject = function (obj, key, value, index) {
-
-	// Create a temp object and index variable
-	var temp = {};
-	var i = 0;
-
-	// Loop through the original object
-	for (var prop in obj) {
-		if (obj.hasOwnProperty(prop)) {
-
-			// If the indexes match, add the new item
-			if (i === index && key && value) {
-				temp[key] = value;
-			}
-
-			// Add the current item in the loop to the temp obj
-			temp[prop] = obj[prop];
-
-			// Increase the count
-			i++;
-
-		}
-	}
-
-	// If no index, add to the end
-	if (!index && key && value) {
-		temp[key] = value;
-	}
-
-	return temp;
-
-};
-
-
-
-// Original object
-var lunch = {
-    sandwich: 'turkey',
-    drink: 'soda',
-    chips: true
-  };
-
-  console.log(lunch)
-  
-  // Add to the end of the object
-addToObject(lunch, 'dessert', 'cookie');
-  
-  // Add between sandwich and drink
-addToObject(lunch, 'topping', 'tomato', 1);
-  
-  // Immutable copy of lunch
-addToObject(lunch);
-
-  console.log(lunch)
--- services/readconf.js
+++ services/readconf.js
@@ -2,8 +2,8 @@ const lineReader = require('line-reader');
 const fs = require('fs');
 const readline = require('readline');
 
-var confnode = {};
 //var confnode = [];
+var confnode = new Map();
 
 console.log("Start readconf.js\n")
 
@@ -11,52 +11,13 @@ try {
     const data = fs.readFileSync('/home/pi/conf.txt', 'UTF-8');
 
     const lines = data.split('\n');
-
     lines.forEach((line) => {
         var w = line.split("\t");
-        var obj = ({ [ w[0] ]: w[1] })
-        var lunchWithDessert = addToObject(confnode, w[0], w[1]);
-        console.log(lunchWithDessert)
+        confnode.set(w[0], w[1])
     });
 } catch (err) {
     console.error(err);
 }
-
-
-var addToObject = function (obj, key, value, index) {
-
-	// Create a temp object and index variable
-	var temp = {};
-	var i = 0;
-
-	// Loop through the original object
-	for (var prop in obj) {
-		if (obj.hasOwnProperty(prop)) {
-
-			// If the indexes match, add the new item
-			if (i === index && key && value) {
-				temp[key] = value;
-			}
-
-			// Add the current item in the loop to the temp obj
-			temp[prop] = obj[prop];
-
-			// Increase the count
-			i++;
-
-		}
-	}
-
-	// If no index, add to the end
-	if (!index && key && value) {
-		temp[key] = value;
-	}
-
-	return temp;
-
-};
-
-
 
 exports.confnode = confnode;
 
