7af0d3930be9e95808b78f3bfde7eed19a77e87b
Carmelo Ricci <carmelo-93@hotmail.it>
2020-07-10 08:58:51 +0200

    ok

---
+++ services/readconf.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+var confnode = new Map();
+
+console.log("Start readconf.js\n")
+
+try {
+    const data = fs.readFileSync('/home/pi/conf.txt', 'UTF-8');
+
+    const lines = data.split('\n');
+    lines.forEach((line) => {
+        var w = line.split("\t");
+        confnode.set(w[0], w[1])
+    });
+} catch (err) {
+    console.error(err);
+}
+
+exports.confnode = confnode;
