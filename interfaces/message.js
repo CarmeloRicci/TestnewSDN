aaa6b603ea1e3be9b49c9c2b6b0e05c3f863aad9
Carmelo Ricci <carmelo-93@hotmail.it>
2020-07-10 14:53:46 +0200

    ok

--- interfaces/message.js
+++ interfaces/message.js
@@ -3,28 +3,39 @@ const ModuleConf = require('./config');
 
 class Message {
 
-    static get_packet_for_message (){
+    static get_packet_for_message() {
         var buf = new Buffer(data);
         var short_name = buf.toString('ascii', 0, 16);
         var name = buf.toString('ascii', 16, 32);
         return ""
     }
 
-    static get_message_for_paket (P){
-        var BNetId =  new Buffer.alloc(ModuleConf.LenNetId,''+[P.NetId],'utf8')
-        var BLength =  new Buffer.alloc(ModuleConf.LenLength,''+[P.Length],'utf8')
-        var BDestination =  new Buffer.alloc(ModuleConf.LenDestination,''+[P.Destination],'utf8')
-        var BSource =  new Buffer.alloc(ModuleConf.LenSource,''+[P.Source],'utf8')
-        var BType =  new Buffer.alloc(ModuleConf.LenType,''+[P.Type],'utf8')
-        var BTTL =  new Buffer.alloc(ModuleConf.LenTTL,''+[P.TTL],'utf8')
-        var BNextHop =  new Buffer.alloc(ModuleConf.LenNextHop,''+[P.NextHop],'utf8')
-        var BPayload =  new Buffer.alloc(ModuleConf.LenPayload,''+[P.Payload],'utf8')
-        //console.log(BNetId.toString(), BLength.toString(), BDestination.toString(), BSource.toString(), BType.toString(), BTTL.toString(), BNextHop.toString(), BPayload.toString())
-        //console.log(BNetId.length, BLength.length, BDestination.length , BSource.length , BType.length , BTTL.length ,BNextHop.length , BPayload.length)
+    static get_message_for_paket(P) {
+
+        var BNetId = Buffer.alloc(ModuleConf.LenNetId)
+        var BLength = Buffer.alloc(ModuleConf.LenLength)
+        var BDestination = Buffer.alloc(ModuleConf.LenDestination)
+        var BSource = Buffer.alloc(ModuleConf.LenSource)
+        var BType = Buffer.alloc(ModuleConf.LenType)
+        var BTTL = Buffer.alloc(ModuleConf.LenTTL)
+        var BNextHop = Buffer.alloc(ModuleConf.LenNextHop)
+        var BPayload = Buffer.alloc(ModuleConf.LenPayload)
+
+        BNetId.write('' + [P.NetId])
+        BLength.write('' + [P.Length])
+        BDestination.write('' + [P.Destination])
+        BSource.write('' + [P.Source])
+        BType.write('' + [P.Type])
+        BTTL.write('' + [P.TTL])
+        BNextHop.write('' + [P.NextHop])
+        BPayload.write('' + [P.Payload])
+
+        console.log(BNetId.toString(), BLength.toString(), BDestination.toString(), BSource.toString(), BType.toString(), BTTL.toString(), BNextHop.toString(), BPayload.toString())
+        console.log(BNetId.length, BLength.length, BDestination.length , BSource.length , BType.length , BTTL.length ,BNextHop.length , BPayload.length)
 
         const TotalLength = BNetId.length + BLength.length + BDestination.length + BSource.length + BType.length + BTTL.length + BNextHop.length + BPayload.length
 
-        const BufferMesage =  Buffer.concat([BNetId, BLength,BDestination,BSource,BType,BTTL,BNextHop,BPayload], TotalLength);
+        const BufferMesage = Buffer.concat([BNetId, BLength, BDestination, BSource, BType, BTTL, BNextHop, BPayload], TotalLength);
 
         return BufferMesage
     }
