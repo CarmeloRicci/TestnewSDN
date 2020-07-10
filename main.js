2a211d12230ea0503a99ca983fa80839c3d3a2b2
Carmelo Ricci <carmelo-93@hotmail.it>
2020-07-10 08:50:16 +0200

    ok

--- main.js
+++ main.js
@@ -7,11 +7,14 @@ async function start() {
 
 var NodeConf = await ModuleReadConf.confnode; // map con tutti i parametri letti dal file
 
-console.log('\n\n\t\t\t Sono il nodo ' + NodeConf.get('MyAddress') +' \n\n')
+console.log('\n\n\t\t I am node ' + NodeConf.get('MyAddress') +'\n\t\tMy Client Address is: '+NodeConf.get('ClientIp')+'\n\t\tMy Server Address is: '+NodeConf.get('ServerIp')+ '\n')
 
 await ModuleListenerC.StartListener("Client Mode",ModuleGetIp.IpClient,2222);
 await ModuleListenerS.StartListener("Station Mode",ModuleGetIp.IpServer,50007);
 
 }
+
+
+
 
 start().then
