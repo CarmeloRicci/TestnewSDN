30c09c61790bf971b37a44959f201c1d389cce67
Carmelo Ricci <carmelo-93@hotmail.it>
2020-07-10 10:11:01 +0200

    ok

--- interfaces/packets.js
+++ interfaces/packets.js
@@ -1,21 +1,21 @@
 class Packets {
     constructor(NetId, Length, Destination, Source, Type, TTL, NextHop, Payload) {
-        this.NetId = NetId
-        this.Length = Length
-        this.Destination = Destination
-        this.Source = Source
-        this.Type = Type
-        this.TTL = TTL
-        this.NextHop = NextHop
-        this.Payload = Payload
+        this.NetId = NetId;
+        this.Length = Length;
+        this.Destination = Destination;
+        this.Source = Source;
+        this.Type = Type;
+        this.TTL = TTL;
+        this.NextHop = NextHop;
+        this.Payload = Payload;
     }
 
     static packets() {
-        return packets
+        return Packets
     }
 
     static print_packets(packets) {
-        return 'NetId: '+packets.NetId+' Length: '+packets.Length+' Destination: '+packets.Destination+ ' Source: ' + packets.Source + ' Type: '+ packets.Type + ' TTL: '+ packets.TTL + ' NextHop: '+ packets.NextHop + ' Payload: ' + packets.Payload;
+        return 'NetId: '+Packets.NetId+' Length: '+Packets.Length+' Destination: '+Packets.Destination+ ' Source: ' + Packets.Source + ' Type: '+ Packets.Type + ' TTL: '+ Packets.TTL + ' NextHop: '+ Packets.NextHop + ' Payload: ' + Packets.Payload;
     }
 }
 
