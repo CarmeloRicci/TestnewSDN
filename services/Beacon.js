b2a653c631403876d903673e569e5e40604b4b0c
Carmelo Ricci <carmeloricci@MacBook-Pro-di-Carmelo.local>
2020-08-02 10:55:36 +0200

    test beacon

--- services/Beacon.js
+++ services/Beacon.js
@@ -5,7 +5,7 @@ const ModuleConf = require('../interfaces/config');
 class Beacon {
 
     static CreateBeaconMessage (Source, Payload) {
-        const p1 = new ModulePackets.Packets(0, 100, 255, Source, 0, 99, 1, Payload);
+        const p1 = new ModulePackets.Packets('0', '100', '255', Source, '0', '99', '1', Payload);
         console.log('CreateBeaconMessage : ' + ModulePackets.Packets.print_packets(p1))
         console.log(p1.NetId.length, p1.Length.length, p1.Destination.length, p1.Source.length, p1.Type.length, p1.TTL.length, p1.NextHop.length, p1.Payload.length)
     
