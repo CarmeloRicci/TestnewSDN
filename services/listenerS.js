b8751cf680d56b7eb63bceb2ba1b44d66618c411
Carmelo Ricci <carmeloricci@MacBook-Pro-di-Carmelo.local>
2020-08-02 17:29:37 +0200

    test beacon

--- services/listenerS.js
+++ services/listenerS.js
@@ -42,7 +42,7 @@ var StartListener = function (TypeListener, NodeConf, FlagRunBeaconProcess) {
   }
 }
 
-var BeaconProcess = function(NodeConf){
+function BeaconProcess (NodeConf){
 
   var message = ModuleMessage.Message.get_message_for_paket(ModuleBeacon.Beacon.CreateBeaconMessage(NodeConf.get('MyAddress'), NodeConf.get('ServerIp')))
   server.send(message, 0, message.length, NodeConf.get('ServerBroadcastPort'), NodeConf.get('ServerBroadcastIp'), function (err, bytes) {
