a4132ac23b824181184427cf36dd9d0b0e014f4e
Carmelo Ricci <carmeloricci@MacBook-Pro-di-Carmelo.local>
2020-08-02 13:15:17 +0200

    test beacon

--- services/listener.js
+++ services/listener.js
@@ -28,7 +28,7 @@ var StartListener = function (TypeListener, NodeConf, FlagRunBeaconProcess) {
 
     server.bind(Port, Ip, function () {
         server.setBroadcast(true);
-        server.addMembership(NodeConf.get('ClientIp'));
+        server.addMembership(NodeConf.get('ClientBroadcast'));
       });
   
 
