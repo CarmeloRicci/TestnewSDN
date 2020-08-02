33fb72eb18a9716b43a054e19346d15809ba8f37
Carmelo Ricci <carmeloricci@MacBook-Pro-di-Carmelo.local>
2020-08-02 10:54:39 +0200

    test beacon

--- services/Beacon.js
+++ services/Beacon.js
@@ -7,7 +7,7 @@ class Beacon {
     static CreateBeaconMessage (Source, Payload) {
         const p1 = new ModulePackets.Packets(0, 100, 255, Source, 0, 99, 1, Payload);
         console.log('CreateBeaconMessage : ' + ModulePackets.Packets.print_packets(p1))
-        console.log(p1.BNetId.length, p1.BLength.length, p1.BDestination.length, p1.BSource.length, p1.BType.length, p1.BTTL.length, p1.BNextHop.length, p1.BPayload.length)
+        console.log(p1.NetId.length, p1.Length.length, p1.Destination.length, p1.Source.length, p1.Type.length, p1.TTL.length, p1.NextHop.length, p1.Payload.length)
     
     }
     static StartBeaconProcess() {
