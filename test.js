79631f071792abe1f51dd9937d64186ef9f4cd5f
Carmelo Ricci <carmeloricci@MacBook-Pro-di-Carmelo.local>
2020-08-02 16:04:45 +0200

    test beacon

--- test.js
+++ test.js
@@ -3,9 +3,7 @@ const NetcatClient = require('netcat/client')
 const nc = new NetcatServer()
 const nc2 = new NetcatClient()
 
-nc.udp().port(2100).listen().on('data', function (rinfo, data) {
+nc.udp().port(5000).listen().on('data', function (rinfo, data) {
     console.log('Got', data.toString(), 'from', rinfo.address, rinfo.port)
     nc.close()
   })
-
-nc2.udp().port(2100).wait(1000).init().send('hello', '127.0.0.1')
