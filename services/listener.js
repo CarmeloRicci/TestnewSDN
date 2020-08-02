1ba48bbce9c8963fcc580e546c7660c9af912125
Carmelo Ricci <carmeloricci@MacBook-Pro-di-Carmelo.local>
2020-08-02 17:28:28 +0200

    test beacon

--- services/listener.js
+++ services/listener.js
@@ -27,7 +27,7 @@ var StartListener = function (TypeListener, NodeConf, FlagRunBeaconProcess) {
         ModulePacketHandler.PacketHandler.packet_handler(ModuleMessage.Message.get_packet_for_message(message)); // Attivo il Packet Handle per il messaggio appena ricevuto
     });
 
-    nc.udp().port( parseInt(NodeConf.get('ClientBroadcastPort')) ).listen().on('data', function (rinfo, data) {
+    nc.udp().port( 0 +parseInt(NodeConf.get('ClientBroadcastPort')) ).listen().on('data', function (rinfo, data) {
         //console.log('Got', data.toString(), 'from', rinfo.address, rinfo.port)
         ModulePacketHandler.PacketHandler.packet_handler(ModuleMessage.Message.get_packet_for_message(data)); // Attivo il Packet Handle per il messaggio appena ricevuto
         nc.close()
