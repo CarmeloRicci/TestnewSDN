ed66657badb93b574e26ef9db7a0509a4c57a826
Carmelo Ricci <carmeloricci@MacBook-Pro-di-Carmelo.local>
2020-08-02 16:58:21 +0200

    test beacon

--- services/listenerS.js
+++ services/listenerS.js
@@ -31,11 +31,11 @@ var StartListener = function (TypeListener, NodeConf, FlagRunBeaconProcess) {
     server.setBroadcast(true);
   });
 
-  nc.udp().port(NodeConf.get('ServerBroadcastPort')).listen().on('data', function (rinfo, data) {
-    console.log('Got', data.toString(), 'from', rinfo.address, rinfo.port)
-    ModulePacketHandler.PacketHandler.packet_handler(ModuleMessage.Message.get_packet_for_message(data)); // Attivo il Packet Handle per il messaggio appena ricevuto
-    nc.close()
-  })
+  // nc.udp().port(NodeConf.get('ServerBroadcastPort')).listen().on('data', function (rinfo, data) {
+  //   console.log('Got', data.toString(), 'from', rinfo.address, rinfo.port)
+  //   ModulePacketHandler.PacketHandler.packet_handler(ModuleMessage.Message.get_packet_for_message(data)); // Attivo il Packet Handle per il messaggio appena ricevuto
+  //   nc.close()
+  // })
 
   if (FlagRunBeaconProcess == 1) {
     console.log("OK 1 !!!")
