5d37526900ae96e074a370784d5ad5c4d99b1377
Carmelo Ricci <carmeloricci@MacBook-Pro-di-Carmelo.local>
2020-08-02 19:22:41 +0200

    test beacon

--- main.js
+++ main.js
@@ -14,14 +14,14 @@ async function start() {
     if (NodeConf.get('Sink') == '1') {
         console.log('\n\t\t I am the Sink\n\n')
         if (NodeConf.get('ServerIp') != undefined)
-            await ModuleListenerS.StartListener("Station Mode", NodeConf, 1);
+            await ModuleListenerS.ListenServer.StartListener("Station Mode", NodeConf, 1);
 
     } else if ((NodeConf.get('Sink') == '0')) {
         console.log('\n\t\t I am a Node\n\n')
         if (NodeConf.get('ClientIp') != undefined)
-            await ModuleListenerC.StartListener("Client Mode", NodeConf, 0);
+            await ModuleListenerC.ListenServer.StartListener("Client Mode", NodeConf, 0);
         if (NodeConf.get('ServerIp') != undefined)
-            await ModuleListenerS.StartListener("Station Mode", NodeConf, 0);
+            await ModuleListenerS.ListenServer.StartListener("Station Mode", NodeConf, 0);
 
     } else {
         console.log('\n\t\t Undefined Node\n\n')
